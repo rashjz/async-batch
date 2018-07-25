49395764c148f57bb60e96e6099c06b511031a47
Rashad Javadov <rashjz@users.noreply.github.com>
2018-07-25 12:15:27 +0200

    Update UserApplication.java

--- src/main/java/com/example/demo/domain/UserApplication.java
+++ src/main/java/com/example/demo/domain/UserApplication.java
@@ -1,14 +1,18 @@
 package com.example.demo.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
 @Builder
+@NoArgsConstructor@AllArgsConstructor
 public class UserApplication implements Serializable {
     private static final long serialVersionUID = 1905122041950251237L;
+    private User user;
     private String applicationID;
     private String departmentID;
 
