712dfc36131572ecb8e970a60ad63df4bc15980d
Rashad Javadov <rashjz@users.noreply.github.com>
2018-07-27 16:54:45 +0200

    Update UserEventListener.java

--- src/main/java/com/example/demo/listener/UserEventListener.java
+++ src/main/java/com/example/demo/listener/UserEventListener.java
@@ -1,8 +1,10 @@
 package com.example.demo.listener;
 
 
+import com.example.demo.collector.UserDataCollector;
 import com.example.demo.event.UserEvent;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
@@ -10,10 +12,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserEventListener {
 
+    private final UserDataCollector userDataCollector;
+
+    @Autowired
+    public UserEventListener(UserDataCollector userDataCollector) {
+        this.userDataCollector = userDataCollector;
+    }
 
     @EventListener
     public void handleUserEvent(UserEvent event) {
         //do some operations
+        userDataCollector.save(event.getUser());
         log.info("data received from listener " + event.getUser().getUsername());
     }
 }
