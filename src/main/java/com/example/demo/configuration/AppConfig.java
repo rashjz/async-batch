98ded79b90b57d5c5cdd7bd245383142cb958417
Rashad Javadov <rashjz@users.noreply.github.com>
2018-08-17 17:15:05 +0200

    Update AppConfig.java

--- src/main/java/com/example/demo/configuration/AppConfig.java
+++ src/main/java/com/example/demo/configuration/AppConfig.java
@@ -1,41 +1,18 @@
 package com.example.demo.configuration;
 
 import com.example.demo.configuration.properties.ApplicationProperties;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.sql.DataSource;
 
 @Configuration
-@EnableTransactionManagement
 public class AppConfig {
 
+
     @Bean
-    @ConfigurationProperties(prefix = "spring.datasource")
+    @ConfigurationProperties(prefix = "application")
     public ApplicationProperties configProperties() {
         return new ApplicationProperties();
     }
 
-    @Bean
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSourceProperties h2DataSourceProperties() {
-        return new DataSourceProperties();
-    }
-
-    @Bean
-    @Qualifier("h2DataSource")
-    public DataSource h2DataSource(@Qualifier("h2DataSourceProperties") final DataSourceProperties properties) {
-        return properties.initializeDataSourceBuilder().build();
-    }
-
-    @Bean
-    public PlatformTransactionManager h2TransactionManager(@Qualifier("h2DataSource") final DataSource dataSource) {
-        return new DataSourceTransactionManager(dataSource);
-    }
 }
