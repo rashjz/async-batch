59525a34279c564051a8afd88cad073c0422c7e5
Rashad Javadov <rashjz@users.noreply.github.com>
2018-08-17 17:15:30 +0200

    Create DataSourceConfig.java

---
+++ src/main/java/com/example/demo/configuration/DataSourceConfig.java
@@ -0,0 +1,57 @@
+package com.example.demo.configuration;
+
+import com.example.demo.configuration.properties.ApplicationProperties;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+public class DataSourceConfig {
+
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.main")
+    public DataSourceProperties mainDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+
+    @Bean
+    @Primary
+    @Qualifier("mainDataSource")
+    public DataSource mainDataSource(@Qualifier("mainDataSourceProperties") final DataSourceProperties properties) {
+        return properties.initializeDataSourceBuilder().build();
+    }
+
+    @Bean
+    public PlatformTransactionManager mainTransactionManager(@Qualifier("mainDataSource") final DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    //test dataSource
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.test")
+    public DataSourceProperties testDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    @Qualifier("testDataSource")
+    public DataSource testDataSource(@Qualifier("testDataSourceProperties") final DataSourceProperties properties) {
+        return properties.initializeDataSourceBuilder().build();
+    }
+
+    @Bean
+    public PlatformTransactionManager testTransactionManager(@Qualifier("testDataSource") final DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+}
